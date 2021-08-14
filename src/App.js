330d2bf2b4416715a224f543aea585b3da9a480e
Nahuel1705 <nahuelmoreno1705@gmail.com>
2021-08-14 09:32:11 -0300

    Messing up the GetCharactersData hook

--- src/App.js
+++ src/App.js
@@ -2,31 +2,45 @@ import CharacterThumbnail from './components/CharacterThumbnail';
 import './assets/styles/global.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import Header from './components/Header';
-import CharactersData from './hooks/CharactersData';
+import GetCharactersData from './hooks/GetCharactersData';
 import RowOfCharacters from './components/RowOfCharacters';
+import { useEffect, useState } from 'react';
 
 const API = 'https://rickandmortyapi.com/api/character'
 
 const formatRowsData = (data, amountOfColumns) => {
   const rows = [];
+  let rowId = 1;
+
   for (let i = 0; i < data.length; i += amountOfColumns) {
     const columns = data.slice(i, i + amountOfColumns);
-    console.log(i, data.length);
-    console.log(columns);
-    rows.push(<RowOfCharacters children={columns.map(column => <CharacterThumbnail key={column.id} {...column}/>)} />)
-    console.log(rows);
+    rows.push(
+      <RowOfCharacters key={rowId}
+        children={
+          columns.map(column => (
+            <CharacterThumbnail key={column.id} {...column}/>
+          ))} 
+        />)
+    rowId ++;
+
   }
+
   return rows
 }
 
+
+
 function App() {
-  const charactersArray = CharactersData(API)
-  console.log(charactersArray)
+  const data = GetCharactersData(API)
+
+  console.log(data)
+  
   return (
     <>
       <Header/>
       <div className="container p-0 mx-auto">
-        {charactersArray?.length && formatRowsData(charactersArray, 6)}
+        {data?.characters?.length > 0 &&
+        formatRowsData(data.characters, 6)}
       </div>
     </>
   );
--- src/components/CharacterThumbnail.jsx
+++ src/components/CharacterThumbnail.jsx
@@ -3,8 +3,16 @@ import 'bootstrap/dist/css/bootstrap.css'
 import '../assets/styles/components/CharacterThumbnail.css'
 const CharacterThumbnail = ({name, location, image}) =>{
     return (
-        <div className="col-2">
-            <img className="img-fluid rounded shadow mx-auto d-block" src={image} alt={name} />
+        <div className="col-2 character">
+            <div className="character-container">
+                <div className="character-overlay">
+                    <div className="overlay-text">
+                        <h4 className="character-name">{name}</h4>
+                        <p className="character-location">{location.name}</p>
+                    </div>
+                </div>
+                <img className="img-fluid shadow mx-auto d-block character-image" src={image} alt={name} />
+            </div>
         </div>
     );
 }
